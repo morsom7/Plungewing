d21d605690d6a7bf60a84c362ee1c840bd958520
HydroCircuit <HydroCircuit@pm.me>
2025-01-25 19:52:27 +0100

    Duck-script

--- escaping_tub/rubber_duck.gd
+++ escaping_tub/rubber_duck.gd
@@ -5,18 +5,18 @@ var control_mode = BOTTLE_MODE
 
 # Upgradables
 @export var cannon_power: float = 1.0 					# Substitute for cannon
-@export var flap_power: float = 1						# Strength of the flap
+@export var flap_power: float = 0.1						# Strength of the flap
 @export var flaps_available: int = 1					# Flapping increases height and allows for more glide
 @export var swimming_rings_available: int = 0			# Gives a bounce if hits the water
+@export var glide_power: float = 1.0						# The amount glide affects trajectory
+@export var is_gliding: bool = false					# checks if duck is gliding
 var is_attached_to_cannon: bool = true						# Checks if duck is launched
 @onready var bottle_duck_anchor: Node2D = $"../FatMan/Launcher/AnimatedSprite2D/Bottle_DuckAnchor"
 @onready var launcher: RigidBody2D = $"../FatMan/Launcher"
 
-
 var is_at_rest: bool = false
 
 func _ready() -> void:	
-	SignalBus.duck_flaps.connect(flap)	# When signal "duck_flaps" is triggered, it calls the function "flap()"
 	SignalBus.duck_bubble_bounced.connect(bounce)
 	SignalBus.bottle_popped.connect(launch)
 	global_position = bottle_duck_anchor.global_position
@@ -31,7 +31,14 @@ func _physics_process(delta: float) -> void:
 			SignalBus.bottle_popped.emit()
 	if control_mode == DUCK_MODE:
 		if Input.is_action_just_pressed("flap_and_glide"):	# Flapping to increase height
-			SignalBus.duck_flaps.emit() 
+			flap()
+			pass
+		if is_gliding:
+			if Input.is_action_pressed("duck_glide_up"):
+				glide_up()
+			if Input.is_action_pressed("duck_glide_down"):
+				glide_down()
+			
 		pass
 	
 	# Checking if movements stops
@@ -43,24 +50,37 @@ func _physics_process(delta: float) -> void:
 func launch():
 	control_mode = DUCK_MODE
 	gravity_scale = 1
-	apply_impulse(Vector2(2000,2000))
+	apply_impulse(Vector2(1000 * cannon_power,-1000 * cannon_power))
 	apply_torque_impulse(500)
 	pass
 
 func flap() -> void:
-	# check if flaps available
-	var force = 4000 # standard force for level 1 flap power
-	apply_impulse(Vector2(0,-force * flap_power))
+	if flaps_available > 0:
+		SignalBus.duck_flaps.emit() 
+		is_gliding = true
+		apply_impulse(Vector2(0,linear_velocity.y * flap_power))
+		
 	# play animation
 	# play sound	
 	# reduce number of flaps available
 	pass
 
-func glide() -> void:
+func glide_up() -> void:
+	
 	# add slight upwards force to counteract gravity
 	# play animation
 	# play sound	
 	pass
-
+	
+func glide_down() -> void:
+	# add slight upwards force to counteract gravity
+	# play animation
+	# play sound	
+	pass
+	
 func bounce(bounce_power: float) -> void:
 	apply_impulse(Vector2(0,-bounce_power))
+	
+func is_hit() -> void:
+	is_gliding = false
+	add_constant_torque(5)
