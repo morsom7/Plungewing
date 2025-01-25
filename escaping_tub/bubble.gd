2b932e01eb934259208f4871e0261c07a8b09046
Kristian Ek <morsom7@hotmail.com>
2025-01-25 22:05:12 +0100

    Working on bubbles

--- escaping_tub/bubble.gd
+++ escaping_tub/bubble.gd
@@ -12,21 +12,12 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	# update animation?
-	# move it randomly around?
-	pass
-
-
-
 func _on_body_entered(body: Node) -> void:
 	# Only detects the player, because the player has Collision = 2, and Bubble has Mask = 2
 	print("Bubble popped")
 	SignalBus.duck_bubble_bounced.emit(bubble_type)
 	# play animation -> animation + fade
 	$FmodBankLoader/SFX_bubble_pop.play_one_shot()
-
 
 #After bubble being popped and SFX stoppped playing, remove bubble:
 func _on_sfx_bubble_pop_stopped() -> void:
