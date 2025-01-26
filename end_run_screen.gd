dc30a343b5e4d9a1bbd09f37912523595548e0ab
HydroCircuit <HydroCircuit@pm.me>
2025-01-26 12:45:06 +0100

    MainMenu

--- end_run_screen.gd
+++ end_run_screen.gd
@@ -38,4 +38,5 @@ func _on_home_button_pressed() -> void:
 
 
 func _on_quit_button_pressed() -> void:
+	get_tree().quit()
 	pass # Replace with function body.
--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,6 +1,24 @@
 extends Node2D
 
-
-
-func _on_button_button_up() -> void:
+func _on_retry_button_button_up() -> void:
+	print("new run")
 	SignalBus.next_escape_attempt.emit()
+	pass # Replace with function body.
+
+
+func _on_shop_button_button_up() -> void:
+	print("shop")
+	SignalBus.open_shop.emit()
+	pass # Replace with function body.
+
+
+func _on_quit_button_button_up() -> void:
+	print("quit")
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_testknapp_pressed() -> void:
+	print("shop")
+	SignalBus.open_shop.emit()
+	pass # Replace with function body.
