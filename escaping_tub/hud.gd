dcbfa2f2b41c358643242d1643cd856e24e7bc89
Kristian Ek <morsom7@hotmail.com>
2025-01-26 13:17:48 +0100

    Fixed UI with info from the beginning

--- escaping_tub/hud.gd
+++ escaping_tub/hud.gd
@@ -4,6 +4,10 @@ func _ready() -> void:
 	SignalBus.duck_flaps.connect(update_flaps)
 	SignalBus.duck_swimming_rings_remaining.connect(update_flaps)
 	SignalBus.currency_updated.connect(update_currency)
+	
+	update_swimming_rings(GameManager.SWIMMING_RINGS)
+	update_flaps(GameManager.FLAP_AMOUNT)
+	update_currency()
 
 func _on_button_button_up() -> void:
 	SignalBus.ui_go.emit()
