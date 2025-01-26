beeca126d7ad1d8735fd31c156a8f104cb1a2c35
HydroCircuit <HydroCircuit@pm.me>
2025-01-26 11:41:40 +0100

    camera

--- escaping_tub/camera_2d.gd
+++ escaping_tub/camera_2d.gd
@@ -1,14 +1,17 @@
 extends Camera2D
 @onready var rubber_duck: RigidBody2D = $"../RubberDuck"
 
+@onready var rubber_duck: RigidBody2D = $"../RubberDuck"
+@export var camera_offset_x: float = 0.0
+@export var camera_offset_y: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.global_position = Vector2(rubber_duck.global_position.x,rubber_duck.global_position.y)
+	self.global_position = Vector2(rubber_duck.global_position.x + camera_offset_x,rubber_duck.global_position.y + camera_offset_y)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	self.global_position = Vector2(rubber_duck.global_position.x,rubber_duck.global_position.y)
+	self.global_position = Vector2(rubber_duck.global_position.x + camera_offset_x,rubber_duck.global_position.y + camera_offset_y)
 	pass
