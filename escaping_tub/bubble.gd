5824601ae3893592d7f500ea41aa007076be378f
morsom7 <morsom7@hotmail.com>
2025-01-26 08:07:45 +0100

    Fixed bubble animation

--- escaping_tub/bubble.gd
+++ escaping_tub/bubble.gd
@@ -18,14 +18,10 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node) -> void:
 	# Only detects the player, because the player has Collision = 2, and Bubble has Mask = 2
-	print("Bubble popped")
 	SignalBus.duck_bubble_bounced.emit(bubble_type)
 	_animated_sprite.play("pop")
-	# play animation -> animation + fade
-	$FmodBankLoader/SFX_bubble_pop.play_one_shot()
 	$Timer.start()
 
 
 func _on_timer_timeout() -> void:
-	print("times up!")
-	pass # Replace with function body.
+	queue_free()
